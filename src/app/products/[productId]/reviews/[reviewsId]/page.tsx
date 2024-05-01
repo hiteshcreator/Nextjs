6cd4f8689653c8a8fdb10501a6b2879bf9539908
hiteshcreator <thehiteshcreator@gmail.com>
2024-05-02 00:15:34 +0530

    nested & all cached segment routing

--- src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,6 +1,12 @@
+import {notFound} from "next/navigation"
+
 const reviewId = ( {params} : { params: { reviewsId:string , productId:string } } ) => {
+
+    if(parseInt(params?.reviewsId) > 1000){
+        notFound();
+    }
   return (
-    <h1>review {params.reviewsId} for Pooduct {params.productId}</h1>
+    <h1>review {params.reviewsId} for Product {params.productId}</h1>
   )
 }
 
