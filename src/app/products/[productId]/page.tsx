cf2f53c90bc5617cb2c33983a34bf83df04d9ac0
hiteshcreator <thehiteshcreator@gmail.com>
2024-05-04 00:06:55 +0530

    Generate Meta Data

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,19 @@
-const ProductId = ({params}:
-    {
-        params:{ productId:string }
-    }
-) => {
+import type { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+  params:{ 
+    productId:string;
+   }
+}
+
+export const generateMetadata = ({params}:Props):Metadata => {
+  return {
+    title: `product ${params.productId}`,
+    description:"Dynamic MetaData"
+  }
+}
+
+const ProductId = ({params}:Props) => {
   return (
     <h1>Details of Products {params.productId}</h1>
   )
