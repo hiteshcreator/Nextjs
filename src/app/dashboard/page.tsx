c889cddd2769982d258a72f1446fa5382bdd85be
hiteshcreator <thehiteshcreator@gmail.com>
2024-05-06 01:10:56 +0530

    jwt install & setup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,54 @@
-const Dashboard = () => {
+// "use client"
+
+// import { useEffect } from "react";
+import jwt from "jsonwebtoken"
+
+const todoData = async() =>{
+
+  try {
+    const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const res = await data.json();
+    console.log(res)
+    return res;
+
+    // .then(response => response.json())
+    // .then(json => console.log(json))
+  } catch (error) {
+    console.log("error",error)
+  }
+   
+}
+
+const Dashboard = async() => {
+
+  const todo:any|[] = await todoData()
+  // jwt
+  // const demo = async()=>{
+  //    todo = await todoData()
+  //    console.log("response",todo)
+  //   // return todo;
+  // }
+  jwt.sign({ data: todo }, process.env.JWT_PRIVATE_KEY,  function(err:any, token:any) {
+    console.log(token,err);
+  });
+  console.log("jwt ",process.env.JWT_PRIVATE_KEY)
+
   return (
     <>
     <div className="sm:container sm:mx-auto ">
 
     <h1>Dashboard</h1>
+    {todo !==undefined ? [todo].map((post:any,i:number)=>(     
+      <>
+      <ul key={i}>
+      <li>{post?.title}hello</li>
+
+      </ul>
+      </>
+      
+
+    )):"dd"
+    }
     </div>
     </>
   )
